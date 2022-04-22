c5cfb8fc877acda23d3e566504dd2878c8ea2b3f
Sergey Bernyakovich <s.bernyakovich@kmtf.kz>
2022-04-22 08:59:48 +0500

    Изменена логика отсчета времени

--- src/modules/timer.mod.js
+++ src/modules/timer.mod.js
@@ -1,79 +1,81 @@
 import { Module } from '../core/module'
 import {
-    getRadialGradient,
-    createElement,
-    getColor,
-    getTextForTimer,
+	getRadialGradient,
+	createElement,
+	getColor,
+	getTextForTimer,
 } from '../utils'
 
 export class TimerModule extends Module {
-    #container
-    #interval
-    #root
+	#container
+	#interval
+	#root
 
-    constructor(el, iconClassName, root) {
-        super(el, iconClassName)
-        this.#container = createElement('div', { className: 'timer' })
-        this.#container.classList.add('fade-right')
-        this.#interval = null
-        this.#root = root
-    }
+	constructor(el, iconClassName, root) {
+		super(el, iconClassName)
+		this.#container = createElement('div', { className: 'timer' })
+		this.#container.classList.add('fade-right')
+		this.#interval = null
+		this.#root = root
+	}
 
-    #createTimer() {
-        this.#container.textContent = ''
-        this.#container.append(
-            createElement('h1', {
-                textContent: 'До Нового 2022 года осталось:',
-            }),
-            createElement('span', {
-                textContent: getTextForTimer('day', [' день', ' дня', ' дней']),
-            }),
-            createElement('span', {
-                textContent: getTextForTimer('hour', [
-                    ' час',
-                    ' часа',
-                    ' часов',
-                ]),
-            }),
-            createElement('span', {
-                textContent: getTextForTimer('minutes', [
-                    ' минута',
-                    ' минуты',
-                    ' минут',
-                ]),
-            }),
-            createElement('span', {
-                textContent: getTextForTimer('second', [
-                    ' секунда',
-                    ' секунды',
-                    ' секунд',
-                ]),
-            }),
-        )
-    }
+	#createTimer() {
+		this.#container.textContent = ''
+		this.#container.append(
+			createElement('h1', {
+				textContent: `До Нового ${
+					new Date().getFullYear() + 1
+				} года осталось:`,
+			}),
+			createElement('span', {
+				textContent: getTextForTimer('day', [' день', ' дня', ' дней']),
+			}),
+			createElement('span', {
+				textContent: getTextForTimer('hour', [
+					' час',
+					' часа',
+					' часов',
+				]),
+			}),
+			createElement('span', {
+				textContent: getTextForTimer('minutes', [
+					' минута',
+					' минуты',
+					' минут',
+				]),
+			}),
+			createElement('span', {
+				textContent: getTextForTimer('second', [
+					' секунда',
+					' секунды',
+					' секунд',
+				]),
+			}),
+		)
+	}
 
-    render() {
-        this.#createTimer()
-        this.el.addEventListener('click', (e) => {
-            this.on = !this.on
-            if (this.on) {
-                this.#root.append(this.#container)
-            }
-            setTimeout(() => {
-                const color = getColor()
-                e.target.classList.add('active')
-                e.target.style.background = getRadialGradient(this.on, color)
-                this.#container.style.color = color
-                if (this.on) {
-                    this.#container.classList.add('active')
-                    this.#interval = setInterval(() => {
-                        this.#createTimer()
-                    }, 1000)
-                } else {
-                    clearInterval(this.#interval)
-                    this.#container.classList.remove('active')
-                }
-            }, 0)
-        })
-    }
+	render() {
+		this.#createTimer()
+		this.el.addEventListener('click', (e) => {
+			this.on = !this.on
+			if (this.on) {
+				this.#root.append(this.#container)
+			}
+			setTimeout(() => {
+				const color = getColor()
+				e.target.classList.add('active')
+				e.target.style.background = getRadialGradient(this.on, color)
+				this.#container.style.color = color
+				if (this.on) {
+					this.#container.classList.add('active')
+					this.#interval = setInterval(() => {
+						this.#createTimer()
+					}, 1000)
+				} else {
+					clearInterval(this.#interval)
+					this.#container.classList.remove('active')
+				}
+			}, 0)
+		})
+	}
 }
--- src/utils.js
+++ src/utils.js
@@ -1,67 +1,68 @@
 import { colors } from './constants'
 
 export function random(min, max) {
-    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+	return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
 
 export function createElement(tagName, attrs) {
-    const elem = document.createElement(tagName)
-    for (const key in attrs) {
-        elem[key] = attrs[key]
-    }
-    return elem
+	const elem = document.createElement(tagName)
+	for (const key in attrs) {
+		elem[key] = attrs[key]
+	}
+	return elem
 }
 
 export function getRadialGradient(on, color) {
-    return on
-        ? 'radial-gradient(' +
-              colors[random(0, colors.length - 1)] +
-              ',' +
-              color +
-              ',' +
-              colors[random(0, colors.length - 1)] +
-              ')'
-        : 'radial-gradient(rgb(0, 212, 131),rgb(255, 152, 0),rgb(255, 193, 7))'
+	return on
+		? 'radial-gradient(' +
+				colors[random(0, colors.length - 1)] +
+				',' +
+				color +
+				',' +
+				colors[random(0, colors.length - 1)] +
+				')'
+		: 'radial-gradient(rgb(0, 212, 131),rgb(255, 152, 0),rgb(255, 193, 7))'
 }
 
 export function getColor() {
-    return colors[random(0, colors.length - 1)]
+	return colors[random(0, colors.length - 1)]
 }
 
 export function getTextForTimer(atr, variants) {
-    const num = getTimerTime(atr)
+	const num = getTimerTime(atr)
 
-    if (num % 10 === 1 && num !== 11) {
-        return num + variants[0]
-    }
-    if ([2, 3, 4].includes(num % 10) && ![12, 13, 14].includes(num)) {
-        return num + variants[1]
-    }
+	if (num % 10 === 1 && num !== 11) {
+		return num + variants[0]
+	}
+	if ([2, 3, 4].includes(num % 10) && ![12, 13, 14].includes(num)) {
+		return num + variants[1]
+	}
 
-    return num + variants[2]
+	return num + variants[2]
 }
 
 function getTimerTime(atr) {
-    const newYearTime = new Date('2022-01-01T00:00:00')
-    const date = new Date()
+	const currentYear = new Date().getFullYear()
+	const newYearTime = new Date(currentYear + 1 + '-01-01T00:00:00')
+	const date = new Date()
 
-    switch (atr) {
-        case 'day': {
-            return Math.floor((newYearTime - date) / (1000 * 60 * 60 * 24))
-        }
-        case 'hour': {
-            return Math.floor(
-                ((newYearTime - date) % (1000 * 60 * 60 * 24)) /
-                    (1000 * 60 * 60),
-            )
-        }
-        case 'minutes': {
-            return Math.floor(
-                ((newYearTime - date) % (1000 * 60 * 60)) / (1000 * 60),
-            )
-        }
-        case 'second': {
-            return Math.floor(((newYearTime - date) % (1000 * 60)) / 1000)
-        }
-    }
+	switch (atr) {
+		case 'day': {
+			return Math.floor((newYearTime - date) / (1000 * 60 * 60 * 24))
+		}
+		case 'hour': {
+			return Math.floor(
+				((newYearTime - date) % (1000 * 60 * 60 * 24)) /
+					(1000 * 60 * 60),
+			)
+		}
+		case 'minutes': {
+			return Math.floor(
+				((newYearTime - date) % (1000 * 60 * 60)) / (1000 * 60),
+			)
+		}
+		case 'second': {
+			return Math.floor(((newYearTime - date) % (1000 * 60)) / 1000)
+		}
+	}
 }
